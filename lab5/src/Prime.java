ea682906744b81321440eb6df9149c8a17face62
badhwarkunal <46719924+badhwarkunal@users.noreply.github.com>
2022-08-22 00:03:53 +0530

    Add files via upload

---
+++ lab5/src/Prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Prime
+{
+	public static void main(String args[])
+	{
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			boolean a=true;
+			for(int i=2;i<=n;i++)
+			{
+				for(int j=2;j<i;j++)
+				{
+					if(i%j==0)
+					{
+						a=false;
+						break;
+					}
+				}
+				if(a==true)
+				{
+					System.out.println(i);
+				}
+		}
+	}
+}
---
+++ lab7/src/SortedMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+public class SortedMap 
+{
+public List getValues(HashMap<String,Integer>s)
+{
+	ArrayList<Integer> a=new ArrayList<>(s.values());
+	Collections.sort(a);
+	return a;
+}
+}
